eabb730820e475b72850e694e4ea4f41bb4d3306
alex <a.caignard@hotmail.fr>
2020-02-17 14:10:48 +0100

    Modif de l'élément updated_at pour qu'il accepte les valeurs nulles (fixtures). Ajout des types d'aliments et affichage

--- src/Entity/Aliment.php
+++ src/Entity/Aliment.php
@@ -78,9 +78,14 @@ class Aliment
     private $lipide;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", nullable=true)
      */
     private $updated_at;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Type", inversedBy="aliments")
+     */
+    private $type;
 
     public function getId(): ?int
     {
@@ -179,6 +184,18 @@ class Aliment
     public function setUpdatedAt(\DateTimeInterface $updated_at): self
     {
         $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getType(): ?Type
+    {
+        return $this->type;
+    }
+
+    public function setType(?Type $type): self
+    {
+        $this->type = $type;
 
         return $this;
     }
--- src/Form/AlimentType.php
+++ src/Form/AlimentType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Aliment;
+use App\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,18 +17,23 @@ class AlimentType extends AbstractType
         $builder
             ->add('nom')
             ->add('prix')
-            ->add('imageFile', FileType::class,['required' => false])
+            ->add('imageFile', FileType::class, ['required' => false])
             ->add('calorie')
             ->add('proteine')
             ->add('glucide')
             ->add('lipide')
-        ;
+            ->add('type', EntityType::class, [
+                'class' => Type::class,
+                'choice_label' => 'libelle'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => Aliment::class,
-        ]);
+        $resolver->setDefaults(
+            [
+                'data_class' => Aliment::class,
+            ]
+        );
     }
 }
